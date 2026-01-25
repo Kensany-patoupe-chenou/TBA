69f39cedcdbc942bae23b475393faaab52219cd4
Kensany-patoupe-chenou <kensany.patoupechenou@edu.esiee.fr>
2026-01-25 21:33:27 +0000

    Include quest

--- character.py
+++ character.py
@@ -1,3 +1,4 @@
+"""Module définissant la classe Character pour le jeu."""
 import random
 
 class Character:
@@ -10,10 +11,11 @@ class Character:
         current_room (Room) : Le lieu où se trouve le personnage.
         msgs (list[str]) : Une liste de messages à afficher lorsque le joueur interroge le PNJ.
     """
-    
-   
 
-    def __init__(self, name, description, current_room, msgs, movement_type):
+
+
+    def __init__(self, name, description, current_room,
+                msgs, *, movement_type="random"):
         """
         Initialise un personnage non-joueur.
 
@@ -22,15 +24,17 @@ class Character:
             description : La description du personnage.
             current_room : La pièce où se trouve le personnage.
             msgs : Une liste de messages que le PNJ peut dire.
+            movement_type : Type de mouvement du personnage ("random"
+                            ou "companion").
         """
         self.name = name
         self.description = description
         self.current_room = current_room
         self.msgs = msgs
-        self.msg_index = 0 
+        self.msg_index = 0
         self._first_meet = True
         self.movement_type = movement_type
-        
+
     def __str__(self):
         """
         Retourne une chaîne de caractères représentant le personnages non joueur.
@@ -39,10 +43,10 @@ class Character:
             str: Une représentation du personnages non joueur au format "(nom, description)".
         """
         return f"{self.name}:{self.description}"
-    
-    
-    def get_msg(self,current_room):
-        
+
+
+    def get_msg(self):
+
         """Retourne le message actuel du personnage."""
         if not self.msgs:
             return "Ce personnage n'a rien à dire."
@@ -50,17 +54,18 @@ class Character:
         msg = self.msgs[self.msg_index]
         self.msg_index = (self.msg_index + 1) % len(self.msgs)
         return msg
-    
+
     def move(self, player_current_room=None):
         """
         Déplace le PNJ aléatoirement dans une pièce adjacente.
         Retourne True si le PNJ s'est déplacé, False sinon.
         """
-        
+
         if self.movement_type == "random":
             # Logique pour Gripsou
             if random.choice([True, False]):
-                possible_rooms = [room for room in self.current_room.exits.values() if room is not None]
+                possible_rooms = [room for room in self.current_room.exits.values()
+                                  if room is not None]
                 if possible_rooms:
                     new_room = random.choice(possible_rooms)
                     if self.name in self.current_room.characters:
@@ -77,5 +82,3 @@ class Character:
                 self.current_room.characters[self.name.lower()] = self
                 return True
         return False
-    
-    
